6fdab960311b1ce2b5c1d77b6dd2a3661a36582c
Mica Horton <mica.e.horton@gmail.com>
2020-12-19 17:29:03 -0800

    Fix router and single product page

---
+++ routes/payment.js
@@ -0,0 +1,30 @@
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+app.post('/create-checkout-session', async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+  
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'Stubborn Attachments',
+              images: ['https://i.imgur.com/EHyR2nP.png'],
+            },
+            unit_amount: 2000,
+          },
+          quantity: 1,
+        },
+  
+      ],
+  
+      mode: 'payment',
+      success_url: `${YOUR_DOMAIN}/success.html`,
+      cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+  
+    });
+  
+    res.json({ id: session.id });
+  
+  });
