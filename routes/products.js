a93e497a0d0cec71e6b770f0a2ce726d82860a5b
Mica Horton <mica.e.horton@gmail.com>
2021-05-11 20:57:50 -0700

    Move to Stripe API backend instead of MongoDB

--- routes/products.js
+++ routes/products.js
@@ -3,31 +3,40 @@ const router = require('express').Router();
 const mongoose = require('mongoose');
 var db = mongoose.connection;
 let checkAdmin = require('../middleware/checkAdmin');
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+
 
 // Normal Routes
-router.route('/').get((req, res) => {
-    Product.find()
-        .then(products => res.json(products))
-        .catch(err => res.status(400).json('Error: ' + err));
+router.route('/').get(async (req, res) => {
+    await stripe.products.list({ limit: 10 })
+        .then(products => {
+            console.log(products);
+            res.json(products);
+        })
+        .catch(err => res.status(400).json('Error: ' + err)); 
 });
 
-router.route('/:id').get((req, res) => {
-    Product.findById(req.params.id)
+router.route('/:id').get(async (req, res) => {
+    await stripe.products.retrieve(req.params.id)
         .then(product => res.json(product))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/price/:id').get(async (req, res) => {
+    await stripe.prices.list({ product: req.params.id })
+        .then(price => res.json(price))
+        .catch(err => res.status(400).json('Error: ' + err)); 
+});
+
+
+/*
 router.route('/category/:category').get((req, res) => {
     Product.find({ category : req.params.category })
         .then(products => res.json(products))
         .catch(err => res.status(400).json('Error: ' + err)); 
 });
 
-router.route('/single/:id').get((req, res) => {
-    Product.findById(req.params.id)
-        .then(product => res.json(product))
-        .catch(err => res.status(400).json('Error: ' + err));
-});
 
 // Protected Routes 
 router.route('/').post(checkAdmin, (req, res, next) => {
@@ -50,7 +59,6 @@ router.route('/').post(checkAdmin, (req, res, next) => {
     newProduct.save()
     .then(() => res.json('Product added!'))
     .catch(err => res.status(400).json('Error: ' + err));
-
 });
 
 router.route('/:id').post(checkAdmin, (req, res, next) => {
@@ -75,6 +83,7 @@ router.route('/:id').delete(checkAdmin, (req, res, next) => {
         .then(() => res.json('Product deleted.'))
         .catch(err => res.status(400).json('Error: ' + err));
 });
+*/
 
 module.exports = router;
 
@@ -104,3 +113,5 @@ router.route('/cart').get(async (req, res) => {
     })
     
 });*/
+
+// Boards (boards), INS Systems (ins), Survey Systems (survey)
